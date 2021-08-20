756fe60ec035ba458f4e2c3cad26650305ba2107
sadman-sakib-abesh <63991795+sadman-sakib-abesh@users.noreply.github.com>
2021-08-20 14:06:00 +0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/firestore';
 import 'firebase/auth';
 //import 'firebase/storage';
 const config={
-    apiKey: "AIzaSyA66VCBovMQx9eJlFN-ppsPiWAWvCrGwhw",
-    authDomain: "claps-bp.firebaseapp.com",
-    projectId: "claps-bp",
-    storageBucket: "claps-bp.appspot.com",
-    messagingSenderId: "544675012413",
-    appId: "1:544675012413:web:53ce05ec5e44a315466f54"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
  }
 
 
